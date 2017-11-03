7c9209305e748b8ce5b2b722c962a8350e98e8e8
Bill Wilder <codingoutloud@gmail.com>
2017-11-03 10:17:33 -0400

    No comment

--- app/js/controllers/UserDetailsController.js
+++ app/js/controllers/UserDetailsController.js
@@ -6,6 +6,8 @@ angular.module('juiceShop').controller('UserDetailsController', [
   function ($scope, $uibModal, userService, id) {
     'use strict'
 
+    // no comment
+    
     userService.get(id).then(function (user) {
       $scope.user = user
     }).catch(function (err) {
